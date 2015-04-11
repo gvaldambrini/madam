b4b0bbb8ae7b65c27440fdb05acdbcf2c53186ff
Gianni Valdambrini <gianni.valdambrini@yahoo.it>
2015-04-11 10:43:46 +0200

    First work in progress version of the form to set workers.

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,11 +11,27 @@ router.get('/settings', function(req, res, next) {
     res.redirect('/settings/workers')
 });
 
+// TODO: save into the database.
+var workers = [];
 
 router.get('/settings/workers', function(req, res, next) {
+
     res.render('settings_workers', {
         isSettingsActive: true,
         isWorkersActive: true,
+        workers: workers.length > 0 ? workers : [''],
+        workersUrl: '#',
+        servicesUrl: '/settings/services'
+    });
+});
+
+router.post('/settings/workers', function(req, res, next) {
+    workers = req.body.name.filter(function(e) { return e; });
+
+    res.render('settings_workers', {
+        isSettingsActive: true,
+        isWorkersActive: true,
+        workers: workers.length > 0 ? workers : [''],
         workersUrl: '#',
         servicesUrl: '/settings/services'
     });
