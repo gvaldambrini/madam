72402719bec511358d16d480f068e28d1307d992
Gianni Valdambrini <gianni.valdambrini@yahoo.it>
2015-04-12 13:53:48 +0200

    Change the es addons.

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ var elasticsearch = require('elasticsearch');
 
 function createClient() {
     if (process.env.NODE_ENV == 'production')
-        return new elasticsearch.Client({host: process.env.SEARCHBOX_URL});
+        return new elasticsearch.Client({host: process.env.BONSAI_URL});
     return new elasticsearch.Client();
 }
 
