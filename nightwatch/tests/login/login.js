59f4d006e3313a1c29642d61442255eb0ac5a2ce
Gianni Valdambrini <gianni.valdambrini@yahoo.it>
2015-06-09 21:52:01 +0200

    Collapse two e2d tests in one.

--- nightwatch/tests/login/login.js
+++ nightwatch/tests/login/login.js
@@ -12,17 +12,12 @@ module.exports = {
       .url('http://localhost:7890');
   },
 
-  'Failed login': function(browser) {
+  'Login': function(browser) {
     browser
       .page.Login().authenticate('admin', 'wrong')
       .page.Login().alertContains('Incorrect password.')
       .page.Login().authenticate('admina', 'pwdadmin')
       .page.Login().alertContains('Incorrect username.')
-      .end();
-  },
-
-  'Successfully login': function(browser) {
-    browser
       .page.Login().authenticate('admin', 'pwdadmin')
       .page.Navbar().loggedUserIs('admin')
       .end();
